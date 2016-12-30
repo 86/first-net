44eb27a0c614c9e2e8d88878438ca51e856b9844
86 <triaedz@gmail.com>
2016-12-31 00:40:06 +0900

    add softmax func

--- activator.py
+++ activator.py
@@ -9,3 +9,11 @@ def sigmoid(x):
 def relu(x):
     return np.maximum(0, x)
 
+def softmax(x):
+    maxx = np.max(x)
+    exp = np.exp(x - maxx)
+    sum_exp = np.sum(exp)
+    y = exp / sum_exp
+    
+    return y
+

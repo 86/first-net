f8a66bb954dbb60e3e0b661f8ff232951c448e95
86 <triaedz@gmail.com>
2017-01-02 02:18:09 +0900

    implement gradient  func

---
+++ gradient.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+def _numerical_gradient_no_batch(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        originX = x[idx]
+
+        x[idx] = originX + h
+        fxh1 = f(x)
+
+        x[idx] = originX - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = originX
+
+    return grad
+
+def numerical_gradient(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_no_batch(f, X)
+    else:
+        grad = np.zeros_like(X)
+
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_no_batch(f, x)
+
+        return grad
+
+def func_x2(x):
+    if x.ndim == 1:
+        return np.sum(x**2)
+    else:
+        return np.sum(x**2, axis=1)
+
+def plot_gradient(f):
+    x0 = np.arange(-2, 2.5, 0.25)
+    x1 = np.arange(-2, 2.5, 0.25)
+    X, Y = np.meshgrid(x0, x1)
+    
+    X = X.flatten()
+    Y = Y.flatten()
+    
+    grad = numerical_gradient(f, np.array([X, Y]))
+    
+    plt.figure()
+    plt.quiver(X, Y, -grad[0], -grad[1],  angles="xy",color="#666666")#,headwidth=10,scale=40,color="#444444")
+    plt.xlim([-2, 2])
+    plt.ylim([-2, 2])
+    plt.xlabel('x0')
+    plt.ylabel('x1')
+    plt.grid()
+    plt.legend()
+    plt.draw()
+    plt.show()
+

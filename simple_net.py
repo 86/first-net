f26a190f6f467967212c16d14bdf0395ccff17e3
86 <triaedz@gmail.com>
2017-01-03 23:55:12 +0900

    implement simple net

---
+++ simple_net.py
@@ -0,0 +1,20 @@
+import sys, os
+import numpy as np
+from activator import *
+from loss_functions import *
+from gradient import *
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+

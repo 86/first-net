7d622fabccc64602b9090ae14516580f9f2bb408
86 <triaedz@gmail.com>
2017-01-03 22:46:42 +0900

    plot gradient decent

--- gradient.py
+++ gradient.py
@@ -36,12 +36,14 @@ def numerical_gradient(f, X):
 
 def gradient_descent(f, init_x, lr = 0.01, step_num = 100):
     x = init_x
+    x_history = []
      
     for i in range(step_num):
+        x_history.append(x.copy())
         grad = numerical_gradient(f, x)
         x -= lr *  grad
      
-    return x 
+    return x, np.array(x_history)
 
 def func_x2(x):
     if x.ndim == 1:
@@ -68,5 +70,18 @@ def plot_gradient(f):
     plt.grid()
     plt.legend()
     plt.draw()
+    plt.show()
+
+def plot_grad_descent(f, init_x, lr, step_num):
+    x, x_history = gradient_descent(f, init_x, lr=lr, step_num=step_num)
+
+    plt.plot( [-5, 5], [0,0], '--b')
+    plt.plot( [0,0], [-5, 5], '--b')
+    plt.plot(x_history[:,0], x_history[:,1], 'o')
+
+    plt.xlim(-3.5, 3.5)
+    plt.ylim(-4.5, 4.5)
+    plt.xlabel("X0")
+    plt.ylabel("X1")
     plt.show()
 
